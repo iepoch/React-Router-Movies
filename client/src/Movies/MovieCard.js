a58bb38d5c9b1928dee7f093c153f349dd6fab9a
iepoch <staclem@live.com>
2018-10-16 21:00:34 -0700

    Initia Commit, added React Router Dom, and some code for MovieCard but not quite working.

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import MovieList from './MovieList';
+import { Link } from 'react-router-dom';
+import Movie from './Movie';
 
 const MovieCard = props => {
-  return;
+
+  const movie = MovieList.map(movie => {
+    return (
+      <Link to={`/movies/${props.id}`}>
+        {movie}
+        </Link>
+      )
+    })
+console.log(movie)
 };
 
 export default MovieCard;
