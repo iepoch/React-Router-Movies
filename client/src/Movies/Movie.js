85eeb4ff9d0927bca8391ac67eb347a0d39fe155
Clem <stacy.clem@e-hps.com>
2018-10-18 07:27:59 -0700

    minor changes

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -26,26 +26,28 @@ export default class Movie extends Component {
         console.error(error);
       });
   };
-  // Uncomment this code when you're ready for the stretch problems
-  componentWillReceiveProps(newProps){
-    if(this.props.match.params.id !== newProps.match.params.id){
-      this.fetchMovie(newProps.match.params.id);
-    }
-  }
 
-  saveMovie = () => {
-    const addToSavedList = this.props.addToSavedList;
-    addToSavedList(this.state.movie)
-  }
+  
+  // Uncomment this code when you're ready for the stretch problems
+  // componentWillReceiveProps(newProps){
+  //   if(this.props.match.params.id !== newProps.match.params.id){
+  //     this.fetchMovie(newProps.match.params.id);
+  //   }
+  // }
+
+  // saveMovie = () => {
+  //   const addToSavedList = this.props.addToSavedList;
+  //   addToSavedList(this.state.movie)
+  // }
 
   render() {
     if (!this.state.movie) {
       return <div>Loading movie information...</div>;
     }
 
-
+    const { title, director, metascore, stars } = this.state.movie;
     return <div className="save-wrapper">
-        <MovieCard {...this.state.movie} saveMovie={this.saveMovie} />
+        <MovieCard {...this.state.movie} />
         {/* <div className="movie-card">
           <h2>{title}</h2>
           <div className="movie-director">
